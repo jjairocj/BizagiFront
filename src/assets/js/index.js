b5936a67ba12a483ca5af484c7a56b1484d5869d
Jairo Cruz <jhon_jairo@live.com>
2018-10-30 20:12:50 -0500

    Add webpack

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,8 @@
+import '../css/style.css';
+
 (function() {
     $(".toggle-sidebar").click(function() {
         $(".left-aside").toggleClass("aside-close");
         $('.main-section').toggleClass('main-collape');
     });
-
-
 })();
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: [
+            "./src/assets/js/routes.js", "./src/assets/js/index.js"
+        ]
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new cleanWebpackPlugin(['dist']),
+        new htmlWebpackPlugin({ template: './index.html' }),
+        new copyWebpackPlugin([{
+            from: './src/components',
+            to: 'components/[name].[ext]',
+            toType: 'template'
+        }]),
+        new copyWebpackPlugin([{
+            from: './src/assets/img',
+            to: 'img/[name].[ext]',
+            toType: 'template'
+        }]),
+        new copyWebpackPlugin([{
+            from: './src/data/books.json',
+            to: 'data/books.json',
+            toType: 'file'
+        }], {
+            ignore: ['*.js', '*.css']
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new webpack.ProvidePlugin({
+            Ractive: ["ractive/ractive.min.js"]
+        })
+
+    ],
+    mode: "production"
+};
